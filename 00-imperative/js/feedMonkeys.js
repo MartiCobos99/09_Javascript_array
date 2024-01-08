b48c8e70fbf21172ee4cfe78bef0ff2a40b0b120
MartiCobos99 <144819011+MartiCobos99@users.noreply.github.com>
2024-01-08 20:09:43 +0100

    first commit

---
+++ 00-imperative/js/feedMonkeys.js
@@ -0,0 +1,21 @@
+/**
+ * Given 'MONKEYS' array, define function 'feedMonkeys' to return an array where each monkey has one fruit
+ * use for() instruction
+ *
+ * expected: with 🍌 => ['🐒 🍌', '🦍 🍌', '🦧 🍌']
+ * expected: with 🍎 => ['🐒 🍎', '🦍 🍎', '🦧 🍎']
+ */
+
+const MONKEYS = ["🐒", "🦍", "🦧"];
+
+export const feedMonkeys = function (fruit) {
+  const monkeys = [];
+  const collectionLength = MONKEYS.length;
+
+  for (let index = 0; index < collectionLength; index++) {
+    const monkey = `${MONKEYS[index]} ${fruit}`;
+    monkeys.push(monkey);
+  }
+
+  return monkeys;
+};
